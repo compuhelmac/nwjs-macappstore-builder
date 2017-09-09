54139abb1ccf90124d3b74413e4f18ab9f3e1b2d
Luis Javier Rodriguez <luis.javier@bamstrategy.com>
2017-09-09 10:51:50 -0400

    0.25 support

--- src/steps/icon.js
+++ src/steps/icon.js
@@ -6,8 +6,8 @@ var m = {};
 
 m.installIcon = function(app_path, icon_path, callback)
 {
-    var rm_command = 'rm "' + app_path + '/Contents/Resources/nw.icns"';
-    var cp_command = 'cp "' + icon_path + '" "' + app_path + '/Contents/Resources/nw.icns"';
+    var rm_command = 'rm "' + app_path + '/Contents/Resources/app.icns"';
+    var cp_command = 'cp "' + icon_path + '" "' + app_path + '/Contents/Resources/app.icns"';
     exec(rm_command + ' && ' + cp_command, function(error)
     {
         callback(error);
